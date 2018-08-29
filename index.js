f0f3e52f0dd763d44f4d11df804c6c9896bf5c2b
ssakethraj <ssakethraj@gmail.com>
2018-08-29 22:18:20 +0530

    add basic route and model

---
+++ index.js
@@ -0,0 +1,49 @@
+//====LIST DEPENDENCIES===//
+const express = require('express');
+const parseurl = require('parseurl');
+const bodyParser = require('body-parser');
+const path = require('path');
+const expressValidator = require('express-validator');
+const mongoose = require('mongoose');
+const Signature = require('./models/signature.js')
+const app = express();
+const url = process.env.MONGO_HOST;
+//=========================//
+
+//====ROOT DIRECTORY===//
+app.get('/', function(req, res) {
+  res.json('you did it');
+});
+//==========================//
+
+//====GET ALL SIGNATURES===//
+app.get('/api/signatures', function(req, res) {
+  Signature.find({}).then(eachOne => {
+    res.json(eachOne);
+    })
+})
+//==========================//
+
+//====POST NEW SIGNATURE===//
+app.post('/api/signatures', function(req, res) {
+    Signature.create({
+      guestSignature: req.body.SignatureOfGuest,
+      message: req.body.MessageofGuest,
+    }).then(signature => {
+      res.json(signature)
+    });
+});
+//==========================//
+
+//====MONGOOSE CONNECT===//
+mongoose.connect(url, function (err, db) {
+    if (err) {
+      console.log('Unable to connect to the mongoDB server. Error:', err);
+    } else {
+      console.log('Connection established to', url);
+    }
+});
+//==========================//
+
+app.listen(process.env.PORT || 3000);
+console.log('starting applicaiton.  Good job!');
